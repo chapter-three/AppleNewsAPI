7f6a1d69cb5b2f3ce820f6f43666032c6c7a1def
Minnur Yunusov <mmyunusov@gmail.com>
2015-07-09 01:07:09 -0700

    Add Unit test skeletons.

---
+++ tests/BaseTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Tests for ChapterThree\AppleNews\PushAPI.
+ */
+
+namespace ChapterThree\AppleNews\PushAPI;
+
+/**
+ * A test class for Base.
+ */
+class BaseTest extends PHPUnit_Framework_TestCase {
+
+  public function testAuthorization() {
+
+  }
+
+}
---
+++ tests/GetTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Tests for ChapterThree\AppleNews\PushAPI.
+ */
+
+namespace ChapterThree\AppleNews\PushAPI;
+
+/**
+ * A test class for Get.
+ */
+class GetTest extends PHPUnit_Framework_TestCase {
+
+  public function testGet() {
+
+  }
+
+}
