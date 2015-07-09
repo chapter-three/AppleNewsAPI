172544019334a891f897dfaa91b67601f3eb5750
Minnur Yunusov <mmyunusov@gmail.com>
2015-07-09 12:03:33 -0700

    Troubleshoot POST method issues.

--- src/PushAPI/Post.php
+++ src/PushAPI/Post.php
@@ -27,7 +27,8 @@ class Post extends Base {
   ];
 
   // Post request data
-  private $data = [];
+  private $boundary;
+  private $contents;
 
   // Multipart data
   private $multipart = [];
@@ -39,7 +40,7 @@ class Post extends Base {
    */
   protected function Authentication(Array $args) {
     $content_type = sprintf('multipart/form-data; boundary=%s', $args['boundary']);
-    $cannonical_request = strtoupper($this->method) . $this->Path() . strval($this->datetime) . $content_type . $args['body'];
+    $cannonical_request = strtoupper($this->method) . $this->Path() . strval($this->datetime) . $content_type . $args['contents'];
     return parent::HHMAC($cannonical_request);
   }
 
@@ -66,37 +67,21 @@ class Post extends Base {
   protected function EncodeMultipartFormdata(Array $file, $encoding = 'binary') {
     $encoded = '';
     foreach ($file as $info) {
-      $encoded .= '--' .  $this->data['boundary'] . static::EOL;
-      $encoded .= static::EOL;
+      $encoded .= '--' .  $this->boundary . static::EOL;
       $encoded .= sprintf('Content-Type: %s', $info['mimetype']) . static::EOL;
       $encoded .= sprintf('Content-Disposition: form-data; filename=%s; name=%s; size=%d', $info['filename'], $info['name'], $info['size']) . static::EOL;
-      if ($info['mimetype'] == 'application/json') {
-        $encoded .= static::EOL;
-        $encoded .= $info['contents'] . static::EOL;
-      }
-      else {
-        $encoded .= sprintf('Content-Transfer-Encoding: %s', $encoding) . static::EOL;
-        $encoded .= static::EOL;
-        if ($encoding == 'binary') {
-          $encoded .= $info['contents'] . static::EOL;
-        }
-        else {
-          $encoded .= chunk_split(base64_encode($info['contents'])) . static::EOL;
-        }
-      }
+      $encoded .= $info['contents'] . static::EOL;
     }
-    $encoded .= '--' .  $this->data['boundary']  . '--' . static::EOL;
+    $encoded .= '--' .  $this->boundary  . '--' . static::EOL;
     $encoded .= static::EOL;
     return $encoded;
   }
 
   protected function EncodeMetadata(Array $metadata) {
     $encoded = '';
-    $encoded .= '--' .  $this->data['boundary'] . static::EOL;
-    $encoded .= static::EOL;
+    $encoded .= '--' .  $this->boundary . static::EOL;
     $encoded .= 'Content-Type: application/json' . static::EOL;
     $encoded .= 'Content-Disposition: form-data; name=metadata' . static::EOL;
-    $encoded .= static::EOL;
     $encoded .= stripslashes(json_encode($metadata, JSON_PRETTY_PRINT)) . static::EOL;
     $encoded .= static::EOL;
     return $encoded;
@@ -108,30 +93,42 @@ class Post extends Base {
 
   public function Post($path, Array $arguments = [], Array $data = []) {
     parent::PreprocessRequest(__FUNCTION__, $path, $arguments);
-    $this->data = $data;
     try {
+      $this->boundary = md5(time());
 
-      $this->data['date'] = !empty($this->data['date']) ? $this->data['date'] : $this->datetime;
-      $this->data['boundary'] = !empty($this->data['boundary']) ? $this->data['boundary'] : md5(time());
-      $this->data['body'] = !empty($this->data['body']) ? $this->data['body'] : '';
+      // Submit JSON string as an article.json file.
+      if (!empty($data['json'])) {
+        $this->multipart[] = $this->FileLoadFormdata([
+          'name' => 'article',
+          'filename' => 'article.json',
+          'mimetype' => 'application/json',
+          'contents' => $data['json'],
+          'size' => strlen($data['json']),
+        ]);
+      }
 
-      foreach ($this->data['files'] as $file) {
+      foreach ($data['files'] as $file) {
         $this->multipart[] = $this->FileLoadFormdata($file);
       }
 
-      $this->data['body'] .= !empty($this->data['metadata']) ? $this->EncodeMetadata($this->data['metadata']) : '';
-      $this->data['body'] .= $this->EncodeMultipartFormdata($this->multipart, 'binary');
+      $this->contents .= !empty($data['metadata']) ? $this->EncodeMetadata($data['metadata']) : '';
+      $this->contents .= $this->EncodeMultipartFormdata($this->multipart);
 
       $this->SetHeaders(
       	[
-          'Accept'          => 'application/json',
-      	  'Authorization'   => $this->Authentication($this->data),
-      	  'Content-Type'    => sprintf('multipart/form-data; boundary=%s', $this->data['boundary']),
-          'Content-Length'  => strlen($this->data['body']),
+      	  'Authorization'   => $this->Authentication(
+            [
+              'boundary' => $this->boundary,
+              'contents' => $this->contents,
+            ]
+          ),
+      	  'Content-Type'    => sprintf('multipart/form-data; boundary=%s', $this->boundary),
+          'Content-Length'  => strlen($this->contents),
       	]
       );
       $this->UnsetHeaders(['Expect']);
-      return $this->Request($this->data['body']);
+      //print_r($this->contents);exit;
+      return $this->Request($this->contents);
     }
     catch (\Exception $e) {
       // Need to write ClientException handling
