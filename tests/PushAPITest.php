a33995353ae69c1540278e9c91b94a1eeb0fc142
Minnur Yunusov <mmyunusov@gmail.com>
2015-07-10 17:49:16 -0700

    Implement __construct test.

--- tests/PushAPITest.php
+++ tests/PushAPITest.php
@@ -5,20 +5,24 @@
  * Tests for ChapterThree\AppleNews.
  */
 
-namespace ChapterThree\AppleNews;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+use org\bovigo\vfs\content\LargeFileContent;
+use Psr\Http\Message\RequestInterface;
 
 /**
  * A test class for PushAPI.
  */
-class BaseTest extends PHPUnit_Framework_TestCase {
+class PushAPITest extends PHPUnit_Framework_TestCase {
 
   const API_KEY_ID = '1e3gfc5e-e9f8-4232-a6be-17bf40edad09';
+  const API_KEY_SECRET = 'qygOz6+eUsIr1j/YkStHUFP2Wv0SbNZ5RStxQ+lagoA=';
   const CHANNEL_ID = '63a75491-2c4d-3530-af91-819be8c3ace0';
   const ENDPOINT = 'https://u48r14.digitalhub.com';
 
   const BASE64_1X1_GIF = 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
   const JSON = '{"key": ["value1", "value2"]}';
-  const BOUNDARY = '093dcc6f56dadd142436f172b5c2124c';
+  const BOUNDARY = '093dcc6f59daad142436f172b5c2124c';
   const POSTDATE = '2015-07-10T12:34:56+00:00';
 
   private $http_client;
@@ -28,6 +32,33 @@ class BaseTest extends PHPUnit_Framework_TestCase {
 
   protected function setUp() {
 
+    // Set up virtual file system.
+    $this->fileroot = vfsStream::setup();
+
+    // Generate file in vfs.
+    $file = vfsStream::newFile('image.gif')
+      ->withContent(base64_decode(static::BASE64_1X1_GIF))
+      ->at($this->fileroot);
+
+    // Add file path to files.
+    $this->files[] = $file->url();
+
+    // Set up PushAPI object.
+    $this->PushAPI = new \ChapterThree\AppleNews\PushAPI(
+      static::API_KEY_ID,
+      static::API_KEY_SECRET,
+      static::ENDPOINT
+    );
+
+  }
+
+  /**
+   * Constructor test.
+   */
+  public function testConstruct() {
+    $this->assertEquals(static::API_KEY_ID, $this->PushAPI->api_key_id);
+    $this->assertEquals(static::API_KEY_SECRET, $this->PushAPI->api_key_secret);
+    $this->assertEquals(static::ENDPOINT,   $this->PushAPI->endpoint);
   }
 
   /**
