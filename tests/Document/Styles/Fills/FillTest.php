7be601c4cc1d2042133851af9c0f7e6eabb7d635
Minnur Yunusov <mmyunusov@gmail.com>
2015-08-09 23:10:53 -0700

    Add tests for Fill abstract class. Add unit tests for VideoFill component.

---
+++ tests/Document/Styles/Fills/FillTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @file
+ * Tests for ChapterThree\AppleNews\Document\Components\Text.
+ */
+
+use ChapterThree\AppleNews\Document\Base;
+use ChapterThree\AppleNews\Document\Styles\Fills\Fill;
+
+/**
+ * A test class for Text.
+ */
+class FillTestClass extends Fill {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($type) {
+    parent::__construct($type);
+  }
+
+}
+
+/**
+ * Tests for the Text class.
+ */
+class FillTest extends PHPUnit_Framework_TestCase {
+
+  /**
+   * Setting properties and outputting json.
+   */
+  public function testSetters() {
+
+    $obj = new FillTestClass('video');
+
+    $json = '{"type":"video"}';
+
+    $this->assertJsonStringEqualsJsonString($json, $obj->json());
+
+    // Optional properties.
+    $json = '{"type":"video","attachment":"fixed"}';
+    $obj->setAttachment('fixed');
+    $this->assertJsonStringEqualsJsonString($json, $obj->json());
+
+  }
+
+}
