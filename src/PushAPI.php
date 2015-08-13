fac5dec107ebd556fe4d382e5c453bff2febe909
Minnur Yunusov <mmyunusov@gmail.com>
2015-08-13 12:59:36 -0700

    Update version to 0.2.2.

--- src/PushAPI.php
+++ src/PushAPI.php
@@ -20,6 +20,6 @@ namespace ChapterThree\AppleNews;
 class PushAPI extends \ChapterThree\AppleNews\PushAPI\Curl {
 
   /** @var (const) PushAPI version */
-  const VERSION = '0.2.0';
+  const VERSION = '0.2.2';
 
 }
