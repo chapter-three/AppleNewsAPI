03f6ae451f37fb9766f10b4247aa62134d1ea8ce
Brian Fisher <tbfisher@gmail.com>
2015-08-14 11:27:08 -0700

    bump

--- src/PushAPI.php
+++ src/PushAPI.php
@@ -11,15 +11,15 @@ namespace ChapterThree\AppleNews;
  * AppleNews PushAPI
  *
  * The PushAPI is a PHP library that allows you to publish content to Apple News.
- * You can also retrieve and delete articles you've already published, 
+ * You can also retrieve and delete articles you've already published,
  * and get basic information about your channel and sections.
- * 
+ *
  * @package    ChapterThree\AppleNews\PushAPI
  * @subpackage ChapterThree\AppleNews\PushAPI\Base
  */
 class PushAPI extends \ChapterThree\AppleNews\PushAPI\Curl {
 
   /** @var (const) PushAPI version */
-  const VERSION = '0.2.3';
+  const VERSION = '0.2.4';
 
 }
