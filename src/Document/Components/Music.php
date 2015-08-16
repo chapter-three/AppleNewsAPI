de95c294e060849f4cb499c91edc44391176487f
Minnur Yunusov <mmyunusov@gmail.com>
2015-08-15 23:32:06 -0700

    Fix Audio component parameter issue.

--- src/Document/Components/Music.php
+++ src/Document/Components/Music.php
@@ -15,16 +15,13 @@ class Music extends Audio {
   /**
    * Implements __construct().
    *
-   * @param mixed $role
-   *   Role.
    * @param mixed $url
    *   URL.
    * @param mixed $identifier
    *   Identifier.
    */
-  public function __construct($role, $url, $identifier = NULL) {
-    parent::__construct('music', $identifier);
-    $this->setUrl($url);
+  public function __construct($url, $identifier = NULL) {
+    parent::__construct('music', $url, $identifier);
   }
 
 }
